60467fdd125493269a4b6f2cad4c3500676edf5d
Robert Hawkey <rhawkey@dal.ca>
2018-05-16 20:22:27 -0300

    [BrokenBuild] I BROKE THE BUILDgit add -Agit add -A

--- src/BrokenBuild/BrokenBuild.java
+++ src/BrokenBuild/BrokenBuild.java
@@ -7,12 +7,10 @@ public class BrokenBuild
 		System.out.println("BrokenBuild! Pull request 2");
 	}
 	
-	/*
 	public static void DoSomethingToBreakTheBuild()
 	{
 		x
 	}
-	*/
 	
 	public static void DoSomethingToBreakRuntime()
 	{

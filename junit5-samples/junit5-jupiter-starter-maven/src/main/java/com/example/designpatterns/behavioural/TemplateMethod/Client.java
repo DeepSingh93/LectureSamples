3f2a7769578a81db183db3d8b88ad2e6e517a4e1
Robert Hawkey <rhawkey@dal.ca>
2018-06-17 22:55:33 -0300

    [DesignPatterns] Fixed Template Method pattern example.

--- junit5-samples/junit5-jupiter-starter-maven/src/main/java/com/example/designpatterns/behavioural/TemplateMethod/Client.java
+++ junit5-samples/junit5-jupiter-starter-maven/src/main/java/com/example/designpatterns/behavioural/TemplateMethod/Client.java
@@ -12,32 +12,10 @@ public class Client
 		movieList.add(new Movie("Star Wars"));
 		movieList.add(new Movie("Air Force One"));
 		movieList.add(new Movie("Back To The Future"));
-		SortMoviesAlphabetically(movieList);
+		Movie.SortMovies(movieList);
 		for (int i = 0; i < movieList.size(); i++)
 		{
 			System.out.println(movieList.get(i).title);
-		}
-	}
-	
-	// Here I'll demonstrate how sorting would work internally
-	// using the template method
-	private void SortMoviesAlphabetically(List<Movie> movieList)
-	{
-		// Crappy bubble sort.
-		for (int i = 0; i < movieList.size(); i++)
-		{
-			for (int j = 1; j < movieList.size() - i; j++)
-			{
-				int indexBeforeJ = j - 1;
-				Movie o1 = movieList.get(indexBeforeJ);
-				Movie o2 = movieList.get(j);
-				// Here's the template method being used.
-				if (o1.compare(o1, o2) > 0)
-				{
-					movieList.remove(indexBeforeJ);
-					movieList.add(j, o1);
-				}
-			}
 		}
 	}
 }
--- junit5-samples/junit5-jupiter-starter-maven/src/main/java/com/example/designpatterns/behavioural/TemplateMethod/Movie.java
+++ junit5-samples/junit5-jupiter-starter-maven/src/main/java/com/example/designpatterns/behavioural/TemplateMethod/Movie.java
@@ -1,6 +1,7 @@
 package com.example.designpatterns.behavioural.TemplateMethod;
 
 import java.util.Comparator;
+import java.util.List;
 
 public class Movie implements Comparator<Movie>
 {
@@ -11,14 +12,36 @@ public class Movie implements Comparator<Movie>
 		this.title = title;
 	}
 	
-	// The Comparator interface built into Java provides us a template method
+	// The Comparator interface built into Java provides us a primitive operation
 	// any class can introduce to then make use of existing Java sort algorithms.
-	// Those algorithms are fixed and never change, no matter what object is passed
-	// to them, because they simply call the compare() template method on that object
-	// knowing it must implement the Comparator interface.
+	// Those algorithms are fixed and never change, they are template methods,
+	// no matter what object is passed to them they simply call the compare() 
+	// method on that object knowing it must implement the Comparator interface.
 	@Override
 	public int compare(Movie o1, Movie o2)
 	{
 		return o1.title.compareToIgnoreCase(o2.title);
+	}
+	
+	// Here is a template method.  The method defines an algorithm with a primitive
+	// operation that can be overriden by subclasses to change the behaviour.
+	public static final void SortMovies(List<Movie> movieList)
+	{
+		// Crappy bubble sort.
+		for (int i = 0; i < movieList.size(); i++)
+		{
+			for (int j = 1; j < movieList.size() - i; j++)
+			{
+				int indexBeforeJ = j - 1;
+				Movie o1 = movieList.get(indexBeforeJ);
+				Movie o2 = movieList.get(j);
+				// Here's the primitive operation that can be overriden.
+				if (o1.compare(o1, o2) > 0)
+				{
+					movieList.remove(indexBeforeJ);
+					movieList.add(j, o1);
+				}
+			}
+		}
 	}
 }

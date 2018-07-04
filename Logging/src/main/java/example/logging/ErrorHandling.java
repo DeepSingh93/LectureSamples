896932e26d8e963536fdf178eed2948de2a2ec20
Robert Hawkey <rhawkey@dal.ca>
2018-07-03 21:23:40 -0300

    [Logging] More examples of logging / error handling.

---
+++ Logging/src/main/java/example/logging/ErrorHandling.java
@@ -0,0 +1,152 @@
+package example.logging;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ErrorHandling
+{
+	private final Logger logger;
+	
+	public ErrorHandling()
+	{
+		logger = LogManager.getLogger(ErrorHandling.class);
+	}
+	
+	public void DoSomethingFatal()
+	{
+		int x = 0;
+		int y = 10 / x;
+	}
+	
+	public void DoSomethingLessFatal(int x)
+	{
+		// Check the input, make sure no divide by zero!
+		if (x == 0)
+		{
+			throw new IllegalArgumentException();
+		}
+		int y = 10 / x;
+	}
+	
+	public void DoSomethingYouCanRecoverFrom(String fileName)
+	{
+		try
+		{
+			try
+			{
+				// Try to write to the given file.
+				WriteToAFile(fileName);
+			}
+			catch (FileNotFoundException e)
+			{
+				/*
+					Note: If you wrote this code this way knowing the file didn't exist
+							beforehand, that would be bad, it would count as using exceptions
+							for control flow. However, this way of programming is useful because
+							you can't always plan for every scenario. It would let you catch a
+							scenario you missed when you first wrote the code, without being fatal
+							to the user.
+				*/
+				// The file doesn't exist, create it, then try again.
+				logger.warn("DoSomethingYouCanRecoverFrom() expected file = " + 
+								fileName + 
+								" to exist, but it did not, creating it now.");
+				CreateFile(fileName);
+				WriteToAFile(fileName);
+			}
+		}
+		catch (IOException e)
+		{
+			// Some other error happened, I didn't plan for that exception, fatal!
+			logger.log(Level.FATAL, "Unknown file IO error writing to file = " + fileName, e);
+		}
+	}
+	
+	public void CreateFile(String fileName) throws IOException
+	{	
+		Path path = Paths.get(fileName);
+		Files.createDirectories(path.getParent());
+		Files.createFile(path);
+	}
+	
+	private void WriteToAFile(String fileName) throws IOException
+	{
+		File f = new File(fileName);
+		if (f.exists())
+		{
+			// This code might also fail, and return a different IOException
+			FileWriter fw = new FileWriter(f);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.append("Hello world!");
+			bw.close();
+			fw.close();
+		}
+		else
+		{
+			throw new FileNotFoundException(fileName + " does not exist!");
+		}
+	}
+	
+	public void CatchAnErrorAtCompileTime()
+	{
+		int x = 0;
+		switch (x)
+		{
+			case 0:
+			{
+				System.out.println("0!");
+			}
+			// Look at the warning to the left that NetBeans gives me.
+			case 1:
+			{
+				System.out.println("1!");
+				break;
+			}
+		}
+		
+		/*
+		boolean y = false;
+		if (y = true)
+		{
+			System.out.println("This one gets through...");
+		}
+		if (true = y)
+		{
+			System.out.println("The compiler catches this one, can't screw this up.");
+		}
+		*/
+	}
+	
+	public void StepThroughYourCode()
+	{
+		List<String> list = new ArrayList<>();
+		list.add("Joe");
+		list.add("Fred");
+		list.add("Steve");
+		list.add("Mike");
+		for (int i = 0; i < list.size(); i++)
+		{
+			DoSomethingToList(list, i);
+			System.out.println(list.get(i));
+		}
+	}
+	
+	private void DoSomethingToList(List<String> list, int index)
+	{
+		if (index > 2)
+		{
+			list.remove(index);
+		}
+	}
+}
--- Logging/src/main/java/example/logging/Main.java
+++ Logging/src/main/java/example/logging/Main.java
@@ -4,12 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main
-{
-	private static final Logger logger = LogManager.getLogger(Main.class);
+{	
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Logging demo!");
+		// Override the default global exception handler for the main thread.
+		GlobalHandler handler = new GlobalHandler();
+		Thread.setDefaultUncaughtExceptionHandler(handler);
+		
+		Logger logger = LogManager.getLogger(Main.class);
 		logger.info("This log level is informational, most likely you would filter it in production.");
 		logger.warn("This log level is a warning, you might keep this in production if the module is very important.");
 		logger.error("This log level is an error, you never want to filter those.");
@@ -17,5 +20,22 @@ public class Main
 		obj.DoSomething();
 		VeryImportantClass obj2 = new VeryImportantClass();
 		obj2.DoSomething();
+		
+		ErrorHandling error = new ErrorHandling();
+		error.StepThroughYourCode();
+		
+		/*
+		ErrorHandling error = new ErrorHandling();
+		error.DoSomethingYouCanRecoverFrom("testfile.txt");
+		try
+		{
+			error.DoSomethingLessFatal(0);		
+		}
+		catch (IllegalArgumentException e)
+		{
+			logger.fatal("Bah I tried to pass zero.", e);
+		}
+		error.DoSomethingFatal();
+		*/
 	}
 }
